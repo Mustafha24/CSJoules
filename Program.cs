b953d2d8ef580727f6aa18802edf69074453f605
Ahmad <112511041+Mustafha24@users.noreply.github.com>
2024-10-15 10:24:32 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace CSharp_Joules
+{
+    class Program{
+        public static void Main(String[] args){
+            Display_Energy de=new Display_Energy();
+
+            de.MeasureEnergy(Test_Snippet,"Test_Snippet");
+            Console.WriteLine("Energy Measurement Completed");
+
+            static void Test_Snippet(){
+                for(int i=0;i<1000000;i++){
+
+                }
+            }
+
+
+        }
+    }
+}
